714e366f20f083c9d2bff5de7007f9d23d70c545
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2026-02-01 21:07:39 +0100

    feat(): header and footer in employment letter

--- src/reports/employment-letter.report.ts
+++ src/reports/employment-letter.report.ts
@@ -1,11 +1,16 @@
-import type { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+import { join } from 'path';
+import type {
+  Content,
+  StyleDictionary,
+  TDocumentDefinitions,
+} from 'pdfmake/interfaces';
 
 const styles: StyleDictionary = {
   header: {
     fontSize: 22,
     bold: true,
     alignment: 'center',
-    margin: [0, 0, 0, 20],
+    margin: [0, 60, 0, 20],
   },
   body: {
     alignment: 'justify',
@@ -15,11 +20,36 @@ const styles: StyleDictionary = {
     fontSize: 14,
     bold: true,
   },
+  footer: {
+    fontSize: 10,
+    italics: true,
+    alignment: 'center',
+    margin: [0, 20, 0, 20],
+  },
+};
+
+const logo: Content = {
+  image: join(process.cwd(), 'src', 'assets', 'tucan-code-logo.png'),
+  width: 100,
+  height: 100,
+  alignment: 'center',
+  margin: [0, 0, 0, 20],
 };
 
 export const getEmploymentLetterReport = (): TDocumentDefinitions => {
   const docDefinition: TDocumentDefinitions = {
     styles,
+    pageMargins: [40, 60, 40, 60],
+    header: {
+      columns: [
+        logo,
+        {
+          text: `${new Date().toLocaleDateString()}`,
+          alignment: 'right',
+          margin: [0, 20, 20, 0],
+        },
+      ],
+    },
     content: [
       {
         text: 'CONSTANCIA DE EMPLEO',
@@ -34,6 +64,10 @@ export const getEmploymentLetterReport = (): TDocumentDefinitions => {
         style: 'signature',
       },
     ],
+    footer: {
+      text: 'Este documento es una constancia de empleo y no representa un compromiso laboral.',
+      style: 'footer',
+    },
   };
 
   return docDefinition;
