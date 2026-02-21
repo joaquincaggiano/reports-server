5d9c3143fe9d2a3c1b9b3894202b0906f6feb2f0
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2026-02-21 14:36:22 +0100

    feat(): add params to chart utils

--- src/helpers/chart-utils.ts
+++ src/helpers/chart-utils.ts
@@ -1,8 +1,26 @@
 import axios from 'axios';
 
-export const chartJsToImage = async (chartConfig: unknown) => {
+interface ChartOptions {
+  height?: number;
+  width?: number;
+}
+
+export const chartJsToImage = async (
+  chartConfig: unknown,
+  options: ChartOptions = {},
+) => {
+  const params = new URLSearchParams();
+
+  if (options.height) {
+    params.append('height', options.height.toString());
+  }
+
+  if (options.width) {
+    params.append('width', options.width.toString());
+  }
+
   const encodedUri = encodeURIComponent(JSON.stringify(chartConfig));
-  const chartUrl = `https://quickchart.io/chart?c=${encodedUri}`;
+  const chartUrl = `https://quickchart.io/chart?c=${encodedUri}&${params.toString()}`;
 
   const response = await axios.get(chartUrl, { responseType: 'arraybuffer' });
 
