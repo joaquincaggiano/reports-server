5ad03cc25e731b46d89c25e40b06b68fb60fcae7
Joaquín Caggiano <joaquincaggiano@MacBook-Pro-de-Joaquin.local>
2026-02-21 17:21:43 +0100

    feat(): replace key in html

--- src/extra-reports/extra-reports.service.ts
+++ src/extra-reports/extra-reports.service.ts
@@ -4,18 +4,27 @@ import { PrinterService } from 'src/printer/printer.service';
 import { getHtmlContent } from 'src/helpers/html-to-pdfmake';
 import { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { headerSection } from 'src/reports/sections/header.section';
+import { footerSection } from 'src/reports/sections/footer.section';
 
 @Injectable()
 export class ExtraReportsService {
   constructor(private readonly printerService: PrinterService) {}
 
   async getHtmlReport() {
-    const html = fs.readFileSync('src/reports/html/basic-01.html', 'utf8');
-    const content = getHtmlContent(html);
+    const html = fs.readFileSync('src/reports/html/basic-02.html', 'utf8');
+    const content = getHtmlContent(html, {
+        client: "Joaquín Caggiano"
+    });
 
     const docDefinition: TDocumentDefinitions = {
       pageMargins: [40, 100, 40, 60],
       header: headerSection({ title: 'HTML to PDFMake' }),
+      footer: (currentPage, pageCount, pageSize) =>
+        footerSection({
+          currentPage: currentPage ?? 1,
+          pageCount: pageCount ?? 1,
+          pageSize: pageSize,
+        }),
       content,
     };
 
--- src/helpers/html-to-pdfmake.ts
+++ src/helpers/html-to-pdfmake.ts
@@ -1,7 +1,18 @@
 import htmlToPdfmake from 'html-to-pdfmake';
 import { JSDOM } from 'jsdom';
 
-export const getHtmlContent = (html: string) => {
+interface ContentReplacer {
+  [key: string]: string;
+}
+
+export const getHtmlContent = (
+  html: string,
+  replacers: ContentReplacer = {},
+) => {
+  Object.entries(replacers).forEach(([key, value]) => {
+    html = html.replaceAll(`{{ ${key} }}`, value);
+  });
+
   const { window } = new JSDOM();
   return htmlToPdfmake(html, { window });
 };
